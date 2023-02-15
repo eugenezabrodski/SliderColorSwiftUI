e5a3025f429cb9c3dd2768049601dcb9dd7c46db
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2023-02-15 23:13:52 +0300

    15.02.2023

--- SliderColorSwiftUI/SliderColorSwiftUI/ContentView.swift
+++ SliderColorSwiftUI/SliderColorSwiftUI/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var valueRed: Double = 0
+    @State private var valueGreen: Double = 0
+    @State private var valueBlue: Double = 0
+    @State private var valueRGB = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            VStack {
+            HStack {
+                Color(red: valueRed, green: valueGreen, blue: valueBlue, opacity: 1)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                Spacer()
+                Color(.red)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+            } .padding(.vertical)
+            VStack {
+                HStack {
+                    Slider(value: $valueRed, in: 0...1, step: 0.01)
+                    .background(.blue)
+                    .cornerRadius(40)
+                    TextField("Color", text: $valueRGB)
+                        .background(.white)
+                        .padding()
+                }
+                HStack {
+                    Slider(value: $valueGreen, in: 0...1, step: 0.01)
+                    .background(.blue)
+                    .cornerRadius(40)
+                TextField("Color", text: $valueRGB)
+                        .background(.white)
+                        .padding()
+                }
+                HStack {
+                Slider(value: $valueBlue, in: 0...1, step: 0.01)
+                    .background(.blue)
+                    .cornerRadius(40)
+                TextField("Color", text: $valueRGB)
+                        .background(.white)
+                        .padding()
+                }
+                Text("\(valueRed)")
+                Button("Save Color") {
+                    Color(red: valueRed, green: valueGreen, blue: valueBlue)
+                }
+            }
+        }
+        }
     }
 }
 
