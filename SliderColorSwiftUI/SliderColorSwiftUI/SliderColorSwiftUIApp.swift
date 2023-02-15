762602784393bc74eae690c76b59f92c5c611346
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2023-02-15 21:40:42 +0300

    15.02.23

---
+++ SliderColorSwiftUI/SliderColorSwiftUI/SliderColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderColorSwiftUIApp.swift
+//  SliderColorSwiftUI
+//
+//  Created by Евгений Забродский on 15.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SliderColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
